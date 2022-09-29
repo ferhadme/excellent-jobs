72d75ffaf0d0ea6a6348df1384e441f57ab4975c
Ferhad Mehdizade <frhdmehdiyev@gmail.com>
2022-09-29 13:49:12 +0400

    init

---
+++ src/main/java/com/ferhad/excellentjobs/ExcellentJobsApplication.java
@@ -0,0 +1,13 @@
+package com.ferhad.excellentjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcellentJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcellentJobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferhad/excellentjobs/ExcellentJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferhad.excellentjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcellentJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
